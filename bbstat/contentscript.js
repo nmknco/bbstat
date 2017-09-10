d71f1e4e84f66ba448dbd66ba49ac4ad5504d676
Lei <nmknco@gmail.com>
2017-09-10 05:38:02 -0400

    Implement basic loading local name dict and searching text for names

--- bbstat/contentscript.js
+++ bbstat/contentscript.js
@@ -1,39 +1,65 @@
 console.log("contentscript executed");
 
-chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse) {
-        console.log("woohoo!");
-        console.log("Received Message: " + message.message);
-        doSomething();
-        sendResponse();
+var nameDict
+var xhr = new XMLHttpRequest();
+var dictURL = chrome.runtime.getURL("data/people.json");
+
+xhr.onload = function() {
+    console.log("name dictionary loaded - response status: %d",
+            xhr.status);
+    console.log("response length: %d", xhr.responseText.length);
+    nameDict = JSON.parse(xhr.responseText);
+    mainfunc();
+}
+
+xhr.open("GET", dictURL, true);
+xhr.send(null);
+
+// need to run below as a call back
+function mainfunc() {
+    chrome.runtime.onMessage.addListener(
+        function(message, sender, sendResponse) {
+            console.log("woohoo!");
+            console.log("Received Message: " + message.message);
+            doSomething();
+            // doSomething2()
+            sendResponse();
+        }
+    );
+}
+
+function doSomethingInner(node) {
+    // console.log("doing something INNER with...");
+    // console.log(node);
+    // console.log(node.childNodes);
+    if (node.nodeType == 3) {
+        var splits = node.data.split(' ');
+        for (var i = 0; i < splits.length; i++) {
+            var word = splits[i];
+            var c = word.charAt(0);
+            if (c >= 'A' && c <= 'Z') {
+                // look for word in the name list
+                if (word in nameDict) {
+                    console.log("found: %s", word);
+                    splits[i] = word + " (PLAYER) ";
+                }
+            }
+        }
+        var highlightedText = splits.join(' ');
+        node.data = highlightedText; // bad practice?
     }
-);
+    else if (node.nodeType == 1 && node.childNodes && 
+            !/(script|style)/i.test(node.tagName)) {
+        var chnds = node.childNodes;
+        for (var i = 0; i < chnds.length; i++) {
+            doSomethingInner(chnds[i]);
+        }
+    }
+}
 
 function doSomething() {
     console.log("doing something...");
-    var articleHtml = $("article").html();
-    // console.log(articleHtml);
-    var keyword = "Cleveland";
-    var splits = articleHtml.split(keyword);
-    console.log(splits.length);
-    var modifiedKey = '<span style="background-color: #FFFF00;">'
-            + keyword + "</span>";
-    console.log(modifiedKey);
-    var highlightedHTML = splits.join(modifiedKey);
-    $("article").html(highlightedHTML);
-}
 
-function findAll(source, target) {
-    indexes = [];
-    for (i = 0; i < source.length - target.length; i++) {
-        var found = true;
-        for (j = 0; j < target.length; j++) {
-            if (source.charAt(i+j) != target.charAt(j)) {
-                found = false;
-                break;
-            }
-        }
-        if (found) indexes.push(i);
-    }
-    return indexes;
+    doSomethingInner(document.body);
+
 }
